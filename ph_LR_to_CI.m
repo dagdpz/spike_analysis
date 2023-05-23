163577a53553061521c7fc26ab29bf50bb008dfa
luschn <lukas.schneider.m@gmail.com>
2023-05-23 15:52:31 +0200

    allowing ecg_bna to use this function without assigning fix_indexes

--- ph_LR_to_CI.m
+++ ph_LR_to_CI.m
@@ -19,17 +19,19 @@ if strcmpi(target(end-1:end),'_R') || strcmpi(target(end-1:end),'_r')
     [pop.trial(hand2).reach_hand]=deal(2);
     
     %% fixation index--> temporary solution
-    fixindex=[pop.trial.fix_index];
-    [pop.trial(fixindex==3).fix_index]=deal(1);
-    [pop.trial(fixindex==1).fix_index]=deal(3);
+    if isfield(pop.trial(1),'fix_index')
+        fixindex=[pop.trial.fix_index];
+        [pop.trial(fixindex==3).fix_index]=deal(1);
+        [pop.trial(fixindex==1).fix_index]=deal(3);
+    end
     
     % invert recording location if perturbation site is left
-%     if strcmp(keys.contra_ipsi_relative_to,'perturbation_site') & strcmpi(target(end-1:end),'_L')
-%         if strcmpi(pop.target(end-1:end),'_L')
-%             pop.target = [pop.target(1:end-1), 'R'];
-%         elseif strcmpi(pop.target(end-1:end),'_R')
-%             pop.target = [pop.target(1:end-1), 'L'];
-%         end
-%     end
+    %     if strcmp(keys.contra_ipsi_relative_to,'perturbation_site') & strcmpi(target(end-1:end),'_L')
+    %         if strcmpi(pop.target(end-1:end),'_L')
+    %             pop.target = [pop.target(1:end-1), 'R'];
+    %         elseif strcmpi(pop.target(end-1:end),'_R')
+    %             pop.target = [pop.target(1:end-1), 'L'];
+    %         end
+    %     end
 end
 end
