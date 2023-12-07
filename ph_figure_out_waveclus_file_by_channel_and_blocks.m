d9896e9b3e1deace21978f6fc9d9539c074a1da8
lubavas <19555010+lubavas@users.noreply.github.com>
2023-12-07 10:49:49 +0100

    use only wc-folder instead of whole cfg

--- ph_figure_out_waveclus_file_by_channel_and_blocks.m
+++ ph_figure_out_waveclus_file_by_channel_and_blocks.m
@@ -1,4 +1,4 @@
-function wc_file_name = ph_figure_out_waveclus_file_by_channel_and_blocks(chNum, blkNum, session_info)
+function wc_file_name = ph_figure_out_waveclus_file_by_channel_and_blocks(chNum, blkNum, session_WC_folder)
 % ph_figure_out_waveclus_file_by_channel_and_blocks
 % This function figures out the corresponding waveclus file that contains
 % data for a unit recorded on the specified channel and blocks. It's
@@ -31,7 +31,7 @@ function wc_file_name = ph_figure_out_waveclus_file_by_channel_and_blocks(chNum,
 chNumStr  = ['00' num2str(chNum)];
 chNumStr  = chNumStr(end-2:end);
 
-concatWCfile = dir([session_info.Input_WC 'concatenation_info.mat']);
+concatWCfile = dir([session_WC_folder 'concatenation_info.mat']);
 concatInfo = load([concatWCfile.folder filesep concatWCfile.name]);
 
 matchBlocks = cellfun(@(x) any(ismember(x, blkNum)), concatInfo.whattofindwhere{chNum}); % check that block lists overlap at least partially
@@ -40,6 +40,6 @@ fileNums = cellfun(@(x) x{chNum}, concatInfo.wheretofindwhat(concatInfo.whattofi
 
 wcFileNum = unique(fileNums);
 
-wc_file_name = [session_info.Input_WC 'dataspikes_ch' chNumStr '_' num2str(wcFileNum) '.mat'];
+wc_file_name = [session_WC_folder 'dataspikes_ch' chNumStr '_' num2str(wcFileNum) '.mat'];
 
 end
