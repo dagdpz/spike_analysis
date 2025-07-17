cb666f08f4ebd2acd57821517ea922d023fc5e45
luschn <lukas.schneider.m@gmail.com>
2025-07-17 17:16:38 +0200

    added mua

--- ph_session_processing.m
+++ ph_session_processing.m
@@ -159,10 +159,7 @@ for current_date = sessions(:)'
         [pop_resorted.monkey]=deal(keys.monkey);
         [pop_resorted.date]=deal(str2double(keys.date));
         pop_resorted = ph_accept_trials_per_unit(pop_resorted,trials,keys);                %% add field accepted for each trial per unit
-        
         pop_resorted = ph_unit_criterias_across_blocks(pop_resorted,trials,keys);                %% add field accepted for each trial per unit
-        
-        
         
         % plot the cells not meeting criteria - not needed any more (?)
         idx_Neuron_ID=DAG_find_column_index(keys.sorting_table,'Neuron_ID');
@@ -221,10 +218,17 @@ function sites = sort_by_site_ID(o_t)
 sites=struct('site_ID',{});
 site_index=0;
 for b=1:size(o_t,2)
-    if isfield(o_t(b).trial(1),'TDT_LFPx')
+    if isfield(o_t(b).trial(1),'TDT_LFPx') || isfield(o_t(b).trial(1),'TDT_MUAx')
+        LFP=[];MUA=[];
+        if isfield(o_t(b).trial(1),'TDT_LFPx')
+            LFP_samples=arrayfun(@(x) size(x.TDT_LFPx,2),o_t(b).trial);
+            LFP=[o_t(b).trial.TDT_LFPx];
+        end
+        if isfield(o_t(b).trial(1),'TDT_MUAx')            
+            MUA_samples=arrayfun(@(x) size(x.TDT_MUAx,2),o_t(b).trial);
+            MUA=[o_t(b).trial.TDT_MUAx];
+        end
         AA=vertcat(o_t(b).trial.channel);
-        LFP=[o_t(b).trial.TDT_LFPx];
-        LFP_samples=arrayfun(@(x) size(x.TDT_LFPx,2),o_t(b).trial);
         n_chans_s=size(AA,2);
         for c=1:n_chans_s
             current_site_already_processed=ismember({sites.site_ID},AA(1,c).site_ID);
@@ -242,32 +246,49 @@ for b=1:size(o_t,2)
                 sites(s).grid_x           =AA(1,c).grid_x;
                 sites(s).grid_y           =AA(1,c).grid_y;
                 sites(s).electrode_depth  =AA(1,c).electrode_depth;
-                sites(s).LFP              =[];
-                sites(s).LFP_samples      =[];
                 sites(s).dataset          =[];
                 %sites(s).perturbation     =[];
                 sites(s).block            =[];
                 sites(s).run              =[];
                 sites(s).n                =[];
+                
+                sites(s).LFP              =[];
+                sites(s).LFP_samples      =[];
                 sites(s).LFP_tStart       =[];
                 sites(s).LFP_t0           =[];
+                
+                
+                sites(s).MUA              =[];
+                sites(s).MUA_samples      =[];
+                sites(s).MUA_tStart       =[];
+                sites(s).MUA_t0           =[];
             end
-            sites(s).LFP_samples   =[sites(s).LFP_samples LFP_samples];
             sites(s).dataset       =[sites(s).dataset      AA(:,c).dataset];
             %sites(s).perturbation  =[sites(s).perturbation AA(:,c).perturbation];
             sites(s).block         =[sites(s).block o_t(b).trial.block];
             sites(s).run           =[sites(s).run   o_t(b).trial.run];
             sites(s).n             =[sites(s).n     o_t(b).trial.n];
+            if ~isempty(LFP)
+            sites(s).LFP_samples   =[sites(s).LFP_samples LFP_samples];
             sites(s).LFP_tStart    =[sites(s).LFP_tStart o_t(b).trial.TDT_LFPx_tStart];
             sites(s).LFP_t0        =[sites(s).LFP_t0 o_t(b).trial.TDT_LFPx_t0_from_rec_start];
             sites(s).LFP           =[sites(s).LFP   LFP(c,:)];
+            end
+            
+            if ~isempty(MUA)
+            sites(s).MUA_samples   =[sites(s).MUA_samples MUA_samples];
+            sites(s).MUA_tStart    =[sites(s).MUA_tStart o_t(b).trial.TDT_MUAx_tStart];
+            sites(s).MUA_t0        =[sites(s).MUA_t0 o_t(b).trial.TDT_MUAx_t0_from_rec_start];
+            sites(s).MUA           =[sites(s).MUA   MUA(c,:)];
+            end
         end
     end
+    
 end
 end
 
 function Trial = sort_by_trial(o_t)
-fieldnames_to_remove={'time_axis','x_eye','y_eye','x_hnd','y_hnd','unit','channel','TDT_LFPx','TDT_CAP1','TDT_ECG1','TDT_ECG4','TDT_POX1'};
+fieldnames_to_remove={'time_axis','x_eye','y_eye','x_hnd','y_hnd','unit','channel','TDT_LFPx','TDT_MUAx','TDT_CAP1','TDT_ECG1','TDT_ECG4','TDT_POX1'};
 Trial=[o_t.trial];
 Trial=rmfield(Trial,fieldnames_to_remove(ismember(fieldnames_to_remove,fieldnames(Trial))));
 
@@ -383,7 +404,8 @@ end
 end
 
 function pop_resorted = sort_by_block(o_t)
-fields_to_remove={'unit','channel','x_eye','y_eye','x_hnd','y_hnd','time_axis','TDT_LFPx','TDT_LFPx_SR','TDT_LFPx_tStart'};
+fields_to_remove={'unit','channel','x_eye','y_eye','x_hnd','y_hnd','time_axis',...
+    'TDT_LFPx','TDT_LFPx_SR','TDT_LFPx_tStart','TDT_MUAx','TDT_MUAx_SR','TDT_MUAx_tStart'};
 % need to fix same block issues.. (-_-)
 block=0;
 for b=1:size(o_t,2)
---
+++ plot_across_time_temp.m
@@ -0,0 +1,176 @@
+function plot_across_time_temp(o,trials,keys,which_units,ch_start_end,whattoplot)
+title_part=[which_units ' ' whattoplot ' over time, ' ch_start_end];
+fig_title=sprintf('%s, session %s, %s',keys.monkey,keys.date,title_part);
+FR_summary_handle     = figure('units','normalized','outerposition',[0 0 1 1],'name',fig_title);
+trial_IDs=[trials.block; trials.run; trials.n]';
+n_columns_rows=ceil(numel(o)^(1/2));
+firstbin=min([trials.run_onset_time]);
+[lastbin,lasttrial_idx]=max([trials.run_onset_time]+[trials.trial_onset_time]);
+lastbin=lastbin+max(trials(lasttrial_idx).states_onset);
+
+tdur=arrayfun(@(x) x.states_onset(x.states==90)-x.states_onset(x.states==2),trials);
+
+units=1:numel(o);
+for u=units
+    trial_blocks=[o(u).block];
+    unique_blocks=unique(trial_blocks);
+    unit_trial_ID=[o(u).block; o(u).run; o(u).n]';
+    UT=trials(ismember(trial_IDs,unit_trial_ID,'rows'));
+    subplot(n_columns_rows,n_columns_rows,u);
+    hold on;
+    binsize=60;
+    AT=[];
+    WF=[];
+    for t=1:numel(o(u).trial)
+        ATt=o(u).trial(t).arrival_times;
+        WFt=o(u).trial(t).waveforms;
+        AT=vertcat(AT,ATt(ATt>0 & ATt<UT(t).states_onset(end-1))+UT(t).trial_onset_time+UT(t).run_onset_time-firstbin);
+        WF=vertcat(WF,WFt);%(ATt>0 & ATt<o(u).trial(t).states_onset(end-1),:));
+    end
+    
+    bins=0:binsize:(lastbin-firstbin);
+    if ismember(whattoplot,{'SNR','amp','noise'})
+        snr=NaN(size(bins));
+        amp=NaN(size(bins));
+        noi=NaN(size(bins));
+        for b=1:numel(bins)
+            idx=AT>bins(b)-binsize/2 & AT<bins(b)+binsize/2;
+            meanwf=mean(WF(idx,:),1);
+            noi(b)=mean(std(WF(idx,:),1));
+            amp(b)=abs(max(meanwf)-min(meanwf));
+            snr(b)=amp(b)/noi(b);
+        end
+    end
+    
+    trial_stability=[o(u).stability_rating];
+    exclusion_reason=[o(u).exclusion_reason];
+    switch whattoplot
+        case 'FR'
+            %% resample per block
+            bins_rs=[];
+            FR_rs=[];
+            FR_rs_c=[];
+            blocks_c=[];
+            N_c=[];
+            for b=unique_blocks
+                btr=[UT.block]==b & ismember([UT.completed],keys.cal.completed) & ~isnan(trial_stability);
+                tb=cumsum(tdur(btr));
+                %% add half the duration here ?
+                tb=[0 tb(1:end-1)]+ [UT(btr).run_onset_time]  - firstbin;
+                FRb=o(u).FR_average(btr);
+                
+                FRrsb = ph_resample_FRs(FRb,tb);
+                FR_rs_c=[FR_rs_c FRrsb];
+                blocks_c=[blocks_c repmat(b,size(FRrsb))];
+                N_c=[N_c numel(FRrsb)];
+                
+                
+                btr=[UT.block]==b;
+                tb=cumsum(tdur(btr));
+                %% add half the duration here ?
+                tb=[0 tb(1:end-1)]+ [UT(btr).run_onset_time]  - firstbin;
+                tb_withITI=cumsum([0 diff([UT(btr).trial_onset_time])]) + [UT(btr).run_onset_time] - firstbin ; 
+                FRb=o(u).FR_average(btr);
+                
+                binrsb= ph_resample_FRs(tb_withITI,tb);
+                FRrsb = ph_resample_FRs(FRb,tb);
+                
+                bins_rs=[bins_rs binrsb];
+                FR_rs=[FR_rs FRrsb];
+            end
+            bins=bins_rs;
+            toplot=FR_rs;
+            toplot_per_trial=[o(u).FR_average];
+            toplot_per_trial(isnan(toplot_per_trial))=0;
+            
+        case 'SNR'
+            toplot=snr;
+            toplot_per_trial=[o(u).SNR_rating];
+        case 'amp'
+            toplot=amp;
+            toplot_per_trial=zeros(numel(o(u).trial),1);
+        case 'noise'
+            toplot=noi;
+            toplot_per_trial=zeros(numel(o(u).trial),1);
+    end
+    plot(bins,toplot,'k','linewidth',0.1);
+    y_lim=ylim(gca);
+    for b=unique_blocks
+        
+        tr_ok=trial_blocks==b;
+        if sum(tr_ok)<2; continue; end;            % it can happen that an entire block is not accepted if FR changed drastically
+        if all([UT(tr_ok).type]==1)
+            style=':';
+        else
+            style='-';
+        end
+        if any(tr_ok & ~isnan(trial_stability))
+            tr_bad = tr_ok & isnan(trial_stability);
+            tr_ok = tr_ok & ~isnan(trial_stability);
+            block_mean=double(nanmean(FR_rs_c(blocks_c==b)));
+        else
+            block_mean=0;
+            tr_bad = tr_ok;
+        end
+        if any(tr_bad)
+            bad_starts=find(diff([false tr_bad])==1);
+            bad_ends  =find(diff([tr_bad false])==-1);
+            % first bit
+            
+            start_block=UT(bad_starts(1)).run_onset_time-firstbin+UT(bad_starts(1)).trial_onset_time;
+            end_block=start_block+UT(bad_ends(1)).trial_onset_time-UT(bad_starts(1)).trial_onset_time;
+            plot([start_block end_block],[0 0],'color',[0.5 0.5 0.5],'linestyle',style,'linewidth',1.5)
+            
+            % second part
+            if numel(bad_starts) == 2                
+                start_block=UT(bad_starts(2)).run_onset_time-firstbin+UT(bad_starts(2)).trial_onset_time;
+                end_block=start_block+UT(bad_ends(2)).trial_onset_time-UT(bad_starts(2)).trial_onset_time;
+                plot([start_block end_block],[0 0],'color',[0.5 0.5 0.5],'linestyle',style,'linewidth',1.5)
+            elseif numel(bad_starts) >2
+                disp('3 invalid intervals for this block ??');
+            end
+        end
+        
+        %FR_std=double(nanstd(FR_smoothed(tr_idx)));
+        start_block=UT(find(tr_ok,1,'first')).run_onset_time-firstbin+UT(find(tr_ok,1,'first')).trial_onset_time;
+        end_block=start_block+UT(find(tr_ok,1,'last')).trial_onset_time-UT(find(tr_ok,1,'first')).trial_onset_time;
+        fanoish_factor=trial_stability(tr_ok);fanoish_factor=fanoish_factor(1);
+        exclusion_code=unique(exclusion_reason(tr_ok));
+        if numel(exclusion_code)>1
+                disp('several reasons to exclude block ??');
+        end
+        switch exclusion_code
+            case 0 %% not excluded
+            col='g';   
+            case 1 %% low FR
+            col=[0.5 0.5 0.5];
+            case 2 %% unstable in general
+            col='r';
+            case 3 %% low spike count
+            col='m';
+            case 4 %% most different FR 
+            col='b';
+             case 5 %% lower stability
+            col='c';
+        end
+        
+        
+        
+        plot([start_block end_block],[block_mean block_mean],'color',col,'linestyle',style,'linewidth',1.5)
+        plot([start_block start_block],[0 block_mean],'color',col,'linestyle',style,'linewidth',1.5)
+        plot([end_block end_block],[0 block_mean],'color',col,'linestyle',style,'linewidth',1.5)
+        text(double(start_block+(end_block-start_block)/2),0,num2str(N_c(unique_blocks==b)),'color',col,'HorizontalAlignment', 'Center')
+        if strcmp(whattoplot,'FR')
+            text(double(start_block+(end_block-start_block)/2), diff(y_lim)/4,sprintf('%0.1f',fanoish_factor),'fontsize',8,'HorizontalAlignment', 'Center')
+        end
+    end
+    unit_title={sprintf('%s %.1f Hz ch/De: %d/%.2f ',o(u).unit_ID,nanmean(o(u).FR_average),o(u).channel,o(u).electrode_depth),...
+        sprintf('b&u: %s',[o(u).block_unit{:}])}; %MP add number of spikes
+    title(unit_title,'interpreter','none','fontsize',6);
+    switch whattoplot
+        case 'FR'
+            set(gca,'xlim',[0,lastbin-firstbin]);
+    end
+end
+ph_title_and_save(FR_summary_handle,fig_title,fig_title,keys)
+end
